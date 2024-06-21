f72034431e806455fda4ab5cf878337201d392ca
TardiCoder <59395097+TardiCoder@users.noreply.github.com>
2024-06-21 12:31:53 -0400

    updated js

--- global.js
+++ global.js
@@ -1,5 +1,27 @@
-console.log("It's alive!");
+console.log("Test");
 
 function $$ (selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
+
+navLinks = $$("nav a");
+let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname)
+currentLink?.classList.add("current");
+
+let pages = [
+    {url: "", title :"Home"},
+    {url: "projects/", title: "Projects"},
+    {url: "contact/", title: "Contact"}
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+}
+
+// Create link and add it to nav
+nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
